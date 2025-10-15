ffbfbf20c2eba6d75744713ac35f5a024bd03839
Luis <hkf24kdns@gmail.com>
2025-10-15 23:49:52 +0200

    add solution 66- sum_mixed_array.js

---
+++ 8 kyu/66- sum_mixed_array.js
@@ -0,0 +1,13 @@
+// exercise: 8 kyu/66- sum_mixed_array.js
+
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+function sumMix(x){
+    return x.reduce((acc, curr) => acc + Number(curr), 0)
+}
+
+console.log(sumMix([9, 3, '7', '3']))
