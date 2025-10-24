9cef6e879e547d439b2606fba6ccb8ae02ae9af9
Luis <hkf24kdns@gmail.com>
2025-10-24 15:57:16 +0200

    add solution 73- count_by_x.js

---
+++ 6 kyu/72- your_order_please.js
@@ -0,0 +1,43 @@
+// exercise: 6 kyu/72- your_order_please.js
+
+/*
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"""  -->  ""
+*/
+
+function order(words){
+    const map = new Map()
+
+    const splitNumberAndWords = (strWithNumber) => {
+        let str = '', number = '';
+
+        for (const el of strWithNumber) {
+            if (/\d/.test(el)) {
+                number += el
+                str += el
+            } else {
+                str += el
+            }
+        }
+
+        return {str, number: Number(number)}
+    }
+
+    words.split(' ').forEach(el => {
+        const {str, number} = splitNumberAndWords(el)
+        map.set(number, str)
+    });
+
+    return Array.from(map).sort((a,b) => a[0] - b[0]).map(el => el[1]).join(' ')
+}
+
+console.log(order("is2 Thi1s T4est 3a"))
+console.log(order("4of Fo1r pe6ople g3ood th5e the2" ))
