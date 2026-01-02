9c4381463ba57875da7eb9b71db76f89cd6216d8
Luis <hkf24kdns@gmail.com>
2026-01-02 16:48:55 +0100

    77- convert_boolean_values_to_strings_yes_or_no.js

---
+++ 8 kyu/77- convert_boolean_values_to_strings_yes_or_no.js
@@ -0,0 +1,9 @@
+// exercise: 8 kyu/77- convert_boolean_values_to_strings_yes_or_no.js
+
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+const boolToWord = ( bool ) => {
+  return bool ? "Yes" : "No"
+}
